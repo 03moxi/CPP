d824870ecd9974c50377bf4caff5c92b1f021bb0
03moxi <moxi_1107@outlook.com>
2025-07-29 21:30:19 +0800

    添加 LinuxDay04 文件夹

---
+++ Linux/LinuxDay04/HelloWorld2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define DEBUG 
+int main(int argc,char*argv[])
+{
+#ifndef DEBUG
+    printf("Hello World!\n");
+    return 0;
+#endif
+    printf("调试代码!\n");
+    return 0;
+}
+
